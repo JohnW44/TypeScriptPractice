a67c22084ac3e6f74ad4c27b2e73335bb0d96e82
JohnW44 <johnwintz@gmail.com>
2025-02-14 10:24:32 -0800

    interface and class

--- greeter.js
+++ greeter.js
@@ -1,6 +1,14 @@
 "use strict";
-function greeter(person) {
-    return "Hello, " + person;
+class Student {
+    constructor(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
 }
-let user = [0, 1, 2];
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+let user = new Student("Jane", "M", "User");
 document.body.textContent = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,9 +1,26 @@
 
+class Student {
+  fullName: string;
+  constructor(
+    public firstName: string,
+    public middleInitial: string,
+    public lastName: string
+  ) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
+}
 
-function greeter(person: string) {
-    return "Hello, " + person;
+interface Person {
+  firstName: string;
+  lastName: string;
+} 
+
+
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
   }
    
-  let user = [0, 1, 2];
+  let user = new Student("Jane", "M", "User");
    
   document.body.textContent = greeter(user);
